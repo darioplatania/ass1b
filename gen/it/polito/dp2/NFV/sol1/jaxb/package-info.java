d966273ba32b3a0e828bf604012eb76fcc14a8c5
darioplatania <dariopl91@gmail.com>
2017-12-09 19:47:57 +0100

    spostata chiave link da element nffge messa sotto con tutte le altre,inoltre rifatto build delle classi

--- gen/it/polito/dp2/NFV/sol1/jaxb/package-info.java
+++ gen/it/polito/dp2/NFV/sol1/jaxb/package-info.java
@@ -2,7 +2,7 @@
 // Questo file xe8 stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.3.0-b170531.0717 
 // Vedere <a href="https://jaxb.java.net/">https://jaxb.java.net/</a> 
 // Qualsiasi modifica a questo file andrxe0 persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2017.12.06 alle 07:26:47 PM CET 
+// Generato il: 2017.12.09 alle 07:42:25 PM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.example.org/nfvInfo", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
